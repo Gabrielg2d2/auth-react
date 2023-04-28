e38bd37313f4ef9415f1723ae800565874b8567c
gabriel_francelino <gabriel.g2d2@gmail.com>
2023-04-28 08:09:34 -0300

    refactor: routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,43 +1,16 @@
-import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { PageHome } from '../pages/Home';
 import { PageLogin } from '../pages/Login';
-import { PrivateRoutes } from './PrivateRoutes';
 import { PageDashboard } from '../pages/Dashboard';
 import { PageAbout } from '../pages/About';
+import { Sidebar } from '../components/Sidebar';
 
-function Sidebar() {
-  const navigation = useNavigate();
-  
-  return (
-    <div className="container">
-      <nav className="sidebar">
-        <h1>Sidebar</h1>
-
-        <ul>
-          <li>
-           <button onClick={() => navigation('/')}>Home - public</button>
-          </li>
-          <li>
-           <button onClick={() => navigation('/dashboard')}>Dashboard</button>
-          </li>
-          <li>
-           <button onClick={() => navigation('/about')}>About</button>
-          </li>
-          <li>
-           <button onClick={() => navigation('/login')}>SignOut</button>
-          </li>
-        </ul>
-      </nav>
-      <PrivateRoutes />
-    </div>
-  )
-}
 
 export function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<PageHome />} />
+        <Route index element={<PageHome />} />
         <Route path="/login" element={<PageLogin />} />
 
         <Route path="/" element={<Sidebar />} >
